d3a623b92015be38a51e9421d5db94066b24c9e4
Doğukan KURNAZ <dogukankurnaz@gmail.com>
2021-09-06 07:57:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+
+const SiteAcilis='15 Oct 2021';
+
+
+function countdown(){
+    const newSiteAcilis = new Date(SiteAcilis);
+    const currentDate = new Date();
+
+
+    const totalSeconds = (newSiteAcilis - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// ilk zaman
+countdown();
+
+setInterval(countdown, 1000);
